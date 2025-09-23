5c3cf214eb8bcf60aa5f781b76bc6aa66c35c0fb
silasttuwei-dot <silasttuwei@gmail.com>
2025-09-23 13:17:49 +0300

    Create PnL.js

---
+++ PnL.js
@@ -0,0 +1,7 @@
+function estimatePnL(buy, sell) {
+  const profit = (sell.price - buy.price).toFixed(6);
+  const roi = ((profit / buy.price) * 100).toFixed(2);
+  return { profit, roi };
+}
+
+module.exports = { estimatePnL };
