5ec6fb324e64733a8286682aac2cfbda39090a73
silasttuwei-dot <silasttuwei@gmail.com>
2025-09-23 13:43:22 +0300

    Update and rename PnL.js to pnl.js

--- pnl.js
+++ pnl.js
@@ -1,4 +1,4 @@
-function estimatePnL(buy, sell) {
+sell estimatePnL(buy, sell) {
   const profit = (sell.price - buy.price).toFixed(6);
   const roi = ((profit / buy.price) * 100).toFixed(2);
   return { profit, roi };
