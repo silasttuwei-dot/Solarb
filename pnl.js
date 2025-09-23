ad7be4127f5f7245027e4f81fc1a0187544d8545
silasttuwei-dot <silasttuwei@gmail.com>
2025-09-23 13:51:30 +0300

    Update pnl.js

--- pnl.js
+++ pnl.js
@@ -1,4 +1,5 @@
-sell estimatePnL(buy, sell) {
+
+function estimatePnL(buy, sell) {
   const profit = (sell.price - buy.price).toFixed(6);
   const roi = ((profit / buy.price) * 100).toFixed(2);
   return { profit, roi };
