3e2865c4cc5f26ba7c66832e70596f71dcf34a96
silasttuwei-dot <silasttuwei@gmail.com>
2025-09-24 13:30:29 +0300

    Delete pnl.js

--- pnl.js
+++
@@ -1,8 +0,0 @@
-
-function estimatePnL(buy, sell) {
-  const profit = (sell.price - buy.price).toFixed(6);
-  const roi = ((profit / buy.price) * 100).toFixed(2);
-  return { profit, roi };
-}
-
-module.exports = { estimatePnL };
