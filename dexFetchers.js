973e4add799da36d114d938a6e89dd95dbdd2645
silasttuwei-dot <silasttuwei@gmail.com>
2025-09-26 13:06:31 +0300

    Delete dexFetchers.js

--- dexFetchers.js
+++
@@ -1,9 +0,0 @@
-// dexPrices.js  (drop-in replacement)
-const { fetchJupiterPrices } = require('./multiRoutePrices');
-
-module.exports = {
-  fetchJupiterPrices,          // live cross-DEX edges
-  fetchRaydiumPairs: fetchJupiterPrices, // stubs so old imports don’t break
-  fetchOrcaPools:    fetchJupiterPrices,
-  fetchSerumMarkets: fetchJupiterPrices
-};
