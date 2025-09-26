3ece50565517cbe571ea263fda1e7c5b154558f5
silasttuwei-dot <silasttuwei@gmail.com>
2025-09-26 13:06:56 +0300

    Delete dexPrices.js

--- dexPrices.js
+++
@@ -1,9 +0,0 @@
-// dexPrices.js
-const { fetchJupiterPrices } = require('./multiRoutePrices');
-
-module.exports = {
-  fetchJupiterPrices,          // live cross-DEX edges
-  fetchRaydiumPairs: fetchJupiterPrices, // stubs for old names
-  fetchOrcaPools:    fetchJupiterPrices,
-  fetchSerumMarkets: fetchJupiterPrices
-};
